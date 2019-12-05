e7320ab7bc38c3b416be0d2b16f3f4a2a6aa86ab
wonntann <atpulley@gmail.com>
2019-12-04 18:01:16 -0800

    Added js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+    var playerBoard = [];
+    var compBoard = [];
+    var compGuessBoard = [];
+    init();
+
+    function init() {
+        playerBoard = [];
+        compBoard = [];
+        compGuessBoard = [];
+        for (var i = 0; i < 10; i++) {
+
+        }
+    }
+});
