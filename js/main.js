24e209bf427061ea6e359fa5e1b665ca139d24c4
wonntann <atpulley@gmail.com>
2019-12-11 17:29:30 -0800

    Added js. Need to add the player, computer, and compGuessBoard rows

--- js/main.js
+++ js/main.js
@@ -9,7 +9,16 @@ $(function() {
         compBoard = [];
         compGuessBoard = [];
         for (var i = 0; i < 10; i++) {
-
+            var row = [];
+            for (var j = 0; i < 10; i++) {
+                row.push("W");
+                var id = i + "_" + j;
+                $("#player").append("<div id='"+id+"' class='gridsquare'></div>");
+                $("#computer").append("<div id='"+id+"' class='gridsquare'></div>");
+//           Add row to the player board
+//           Add row to computer board
+//           Add row to compGuessBoard board
+            }
         }
     }
 });
