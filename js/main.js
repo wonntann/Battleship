1bd112f2ae58114bffdd2b14d653c57cae2eedf0
wonntann <atpulley@gmail.com>
2020-01-27 11:30:55 -0800

    Working on player guesses need to add the rest of the drawing on the board for the player guesses.

--- js/main.js
+++ js/main.js
@@ -16,31 +16,90 @@ $(function() {
            var placed = false;
            var vertical = Math.round(Math.random()) == 0;
            while(!placed) {
-            if(vertical) {
-                var c = Math.floor(Math.random() * this.board.length);
-                var r = Math.floor(Math.random() * (this.board.length-length));
-                for (var i=0; i < length; i++) {
-                    this.squares.push([r + i, c]);
-                    if(this.board[r+i][c] == "S") {
-                        this.squares = [];
-                        break;
-                    }
-                }
-            } else { //Left off on 12/18/19
-
-            }
+               if(vertical) {
+                   var c = Math.floor(Math.random() * this.board.length);
+                   var r = Math.floor(Math.random() * (this.board.length-length));
+                   for (var i=0; i < length; i++) {
+                       this.squares.push([r + i, c]);
+                       if(this.board[r+i][c] == "S") {
+                           this.squares = [];
+                           break;
+                       }
+                   }
+           } else {
+               var r = Math.floor(Math.random() * this.board.length);
+               var c = Math.floor(Math.random() * (this.board.length - length));
+               for (var i = 0; i < length; i++) {
+                   this.squares.push([r, c + i]);
+                   if (this.board[r][c + i] == "S") {
+                       this.squares = [];
+                       break;
+                   }
+               }
+           }
+           if(this.squares.length == length) {
+               placed = true;
+               for (var i = 0; i < this.squares.length; i++) {
+                   var r = this.squares[i][0];
+                   var c = this.squares[i][1];
+                   this.board[r][c] = "S";
+               }
            }
         }
     }
+    this.place();
+} /* End function Ship */
+
+      /*#### Add function checkSunk 1/15/20 ##### */
+    /* How to tell if the ship is sunk*/
+
+         this.checkSunk = function(){
+             /* Iterates through the ship's list of quares and checks the cooresponding position in the board */
+            for (var i = 0; i< this.squares.length; i++){
+                var r = this.squares[i][0];
+                var c = this.squares[i][1];
+                /* If a square's calue indicates part of a ship, we  know that the ship is not sunk and returns false */
+                if(this.board[r][c] === "S"){
+                    return false;
+                }
+            }
+             /* If the loop completes, we know that the ship is sunk and can set the ship's sunk variable to true and return true. */
+            this.sunk = true;
+            return true;
+         }
+}
+
+    function drawBoard(board, player) {
+        for (var i = 0; i < board.length; i++) {
+            for (var j = 0; j < board[i].length; j++) {
+                var color = "#1AD1FF";
+                if(board[i][j] == "S" && player === "player") {
+                    color = "gray";
+                } /* Use different colors to indicate hits and misses */
+                else if(board[i][j] == "H"){
+                    color = "#e60000";
+                }else if(board[i][j] == "M"){
+                    color = "#ffff1a";
+                }
+                $("#" + player + " > #" + i + "_" + j).css("background-color", color);
+            }
+        }
+    }
+/* Function to see checkWon */
+
+
+/* Function compTurn for computer turn */
+
 
     function init() {
         playerBoard = [];
-
+        playerShips = [];
         compBoard = [];
         compGuessBoard = [];
+        compShips = [];
         for (var i = 0; i < 10; i++) {
             var row = [];
-            for (var j = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
                 row.push("W");
                 var id = i + "_" + j;
                 $("#player").append("<div id='"+id+"' class='gridsquare'></div>");
@@ -53,5 +112,26 @@ $(function() {
 //           Add row to compGuessBoard board
             compGuessBoard.push(row.slice());
             }
+
+            /* Ended off class 1/8/20 */
+            playerShips.push(new Ship("Carrier", 5, playerBoard));
+            playerShips.push(new Ship("Battleship", 4, playerBoard));
+            playerShips.push(new Ship("Cruiser", 3, playerBoard));
+            playerShips.push(new Ship("Submarine", 3, playerBoard));
+            playerShips.push(new Ship("Destroyer", 2, playerBoard));
+            compShips.push(new Ship("Carrier", 5, compBoard));
+            compShips.push(new Ship("Battleship", 4, compBoard));
+            compShips.push(new Ship("Cruiser", 3, compBoard));
+            compShips.push(new Ship("Submarine", 3, compBoard));
+            compShips.push(new Ship("Destroyer", 2, compBoard));
+
+            drawBoard(playerBoard, "player");
+
+            /* Click Event on class gridSquare */
+            $("#computer > .gridsquare").click(function(){
+                var id = $(this).attr("id").split("_");
+                var r = parseInt(id[0]);
+                var c = parseInt(id[1]);
+                /******#### END 1/22 ####*****/
         }
 });
