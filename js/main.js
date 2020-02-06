dd452ee159cfb5b8b2573e85fac7dd14cf96af09
wonntann <atpulley@gmail.com>
2020-02-05 17:21:57 -0800

    added the computer turn

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(function() {
     var playerShips = [];
     var compBoard = [];
     var compShips = [];
+
     var compGuessBoard = [];
     init();
 
@@ -97,6 +98,24 @@ $(function() {
 
 /* Function compTurn for computer turn */
 function compTurn(){
+    var r = 0;
+    var c = 0;
+    do {
+        r = Math.floor(Math.random() * compGuessBoard.length);
+        c = Math.floor(Math.random() * compGuessBoard.length);
+    } while(compGuessBoard[r][c] !== "W")
+    if(playerBoard[r][c] === "S") {
+        playerBoard[r][c] = "H";
+        compGuessBoard[r][c] = "H";
+        $('#ptext').html("Hit!");
+        for (var i = 0; i < playerShips.length; i++) {
+            if(!playerShips[i].sunk && playerShips[i].checkSunk()) {
+                $('#ptext').html('Your ' + playerShips[i].name + 'was sunk!');
+                break;
+            }
+        }
+    } else /* ############### Ended off on class 2/5/2020 ###########*/
+}
 
     function init() {
         playerBoard = [];
